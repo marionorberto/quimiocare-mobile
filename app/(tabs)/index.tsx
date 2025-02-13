5c3302d66c1ad0ba1b3ab6587c9655fb5e724ba8
marionorberto <marionorberto2018@gmail.com>
2025-02-13 14:09:21 +0100

    test: mobile

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Mário Norberto!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
